460c6005f7f707a5e2ff74129289a957aec5c71f
Justin Harden <bionicle4365@gmail.com>
2025-08-15 13:29:32 -0400

    access

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,6 +3,8 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.0"
+      AWS_ACCESS_KEY_ID     = var.aws_access_key
+      AWS_SECRET_ACCESS_KEY = var.aws_secret_access_key
     }
   }
   backend "s3" {}
