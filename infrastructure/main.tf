1ff1b0dfc6ef9413b7b2716d451edf4f029eed3a
Justin Harden <bionicle4365@gmail.com>
2025-08-15 13:38:05 -0400

    case

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -9,8 +9,8 @@ terraform {
 }
 
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_access_key
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 module "ecr" {
