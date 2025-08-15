79fb4dd21b43cffd14cf3dc07ea602be0601afad
Justin Harden <bionicle4365@gmail.com>
2025-08-15 13:50:54 -0400

    fix9

--- infrastructure/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "AWS_ACCESS_KEY" {
-  description = "AWS access key ID"
-  type        = string
-}
-
-variable "AWS_SECRET_ACCESS_KEY" {
-  description = "AWS secret access key"
-  type        = string
-}
