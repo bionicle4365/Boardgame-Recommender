20b3f930b5808e8e406e4e1559483e0f8af191e8
Justin Harden <bionicle4365@gmail.com>
2025-08-15 21:14:14 -0400

    filename

--- infrastructure/lambda/main.tf
+++ infrastructure/lambda/main.tf
@@ -1,10 +1,11 @@
 resource "aws_lambda_function" "bgg_game_data_scraper" {
-  function_name = var.lambda_function_name
-  runtime       = "python3.11"
-  handler      = "bgg_game_data_scraper.lambda_handler"
-  role         = var.lambda_execution_role_arn
-  timeout      = 300 # 5 minutes
+  function_name    = var.lambda_function_name
+  runtime          = "python3.10"
+  handler          = "bgg_game_data_scraper.lambda_handler"
+  role             = var.lambda_execution_role_arn
+  timeout          = 300 # 5 minutes
   source_code_hash = filebase64sha256("bgg_game_data_scraper.zip")
+  filename         = "bgg_game_data_scraper.zip"
 }
 
 resource "aws_lambda_event_source_mapping" "bgg_game_data_scraper_esm" {
