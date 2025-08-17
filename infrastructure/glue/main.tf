eb7ede2cc2067f8dbabd5ec487d3da72a01c46f5
Justin Harden <bionicle4365@gmail.com>
2025-08-17 10:11:34 -0400

    got the types

--- infrastructure/glue/main.tf
+++ infrastructure/glue/main.tf
@@ -27,59 +27,89 @@ resource "aws_glue_catalog_table" "boardgame_app_table" {
     }
 
     columns {
-      name = "id"
-      type = "string"
+        name = "id"
+        type = "string"
     }
 
     columns {
-      name = "type"
-      type = "string"
+        name = "type"
+        type = "string"
     }
 
     columns {
-      name = "name"
-      type = "string"
+        name = "name"
+        type = "string"
     }
 
     columns {
-      name = "year_published"
-      type = "int"
+        name = "year_published"
+        type = "int"
     }
 
     columns {
-      name = "min_players"
-      type = "int"
+        name = "min_players"
+        type = "int"
     }
 
     columns {
-      name = "max_players"
-      type = "int"
+        name = "max_players"
+        type = "int"
     }
 
     columns {
-      name = "playing_time"
-      type = "int"
+        name = "playing_time"
+        type = "int"
     }
 
     columns {
-      name = "description"
-      type = "string"
+        name = "description"
+        type = "string"
     }
     
     columns {
-      name = "min_playtime"
-      type = "int"
+        name = "min_playtime"
+        type = "int"
     }
 
     columns {
-      name = "max_playtime"
-      type = "int"
+        name = "max_playtime"
+        type = "int"
     }
 
     columns {
-      name = "min_age"
-      type = "int"
+        name = "min_age"
+        type = "int"
     }
+
+    columns {
+        name = "categories"
+        type = "array<string>"
+      }
+
+    columns {
+        name = "mechanics"
+        type = "array<string>"
+      }
+
+    columns {
+        name = "designers"
+        type = "array<string>"
+      }
+
+    columns {
+        name = "artists"
+        type = "array<string>"
+      }
+
+    columns {
+        name = "publishers"
+        type = "array<string>"
+      }
+
+    columns {
+        name = "families"
+        type = "array<string>"
+      }
     
   }
 }
