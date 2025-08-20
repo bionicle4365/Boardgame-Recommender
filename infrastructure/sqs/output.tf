85d56f4e9e6dce27d7c1acf1ea54888fe34ca55e
Justin Harden <bionicle4365@gmail.com>
2025-08-20 09:51:25 -0400

    fix

--- infrastructure/sqs/output.tf
+++ infrastructure/sqs/output.tf
@@ -1,8 +1,8 @@
-output "sqs_queue_url" {
+output "data_sqs_queue_url" {
   description = "The URL of the SQS queue for the BGG game data scraper"
   value       = aws_sqs_queue.bgg_game_data_scraper_queue.id
 }
-output "sqs_queue_arn" {
+output "data_sqs_queue_arn" {
   description = "The ARN of the SQS queue for the BGG game data scraper"
   value       = aws_sqs_queue.bgg_game_data_scraper_queue.arn
 }
