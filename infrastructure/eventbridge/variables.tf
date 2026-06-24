cf37a12eaeeddae5356ad22ee7339c4fe92cba22
Justin Harden <bionicle4365@gmail.com>
2026-06-24 10:55:44 -0400

    feat: add Terraform variables for ECS cluster and compactor lambda configuration

--- infrastructure/eventbridge/variables.tf
+++ infrastructure/eventbridge/variables.tf
@@ -1,8 +1,9 @@
-variable "ecs_cluster_arn" { type = string }
+﻿variable "ecs_cluster_arn" { type = string }
 variable "ecs_task_definition_arn" { type = string }
 variable "ecs_subnets" { type = list(string) }
 variable "ecs_security_group_id" { type = string }
 variable "ecs_task_execution_role_arn" { type = string }
 variable "ecs_task_role_arn" { type = string }
 variable "compactor_lambda_arn" { type = string }
-variable "compactor_lambda_name" { type = string }
+variable "compactor_lambda_name" { type = string }
+
